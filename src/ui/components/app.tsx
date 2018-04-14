8321cc7677779ad7b0911f4b82325f3db2332727
Anders Liu <anders.liu@gmail.com>
2018-04-14 00:57:42 -0700

    Basic build facility of ui bundle.

---
+++ src/ui/components/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export class App extends React.Component {
+    public render(): JSX.Element {
+        return (
+            <div>Welcome to PEViewer</div>
+        )
+    }
+}
---
+++ src/ui/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as Redux from "redux";
+import * as ReactRedux from "react-redux";
+
+import { App } from "./components/app";
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
